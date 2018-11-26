c0e236207a35292426aaf3ca0c804b3073ea93c8
ohjongsung <ohjongsung@gmail.com>
2018-11-26 22:46:37 +0900

    전략 api client 기능 추가

--- cybos/cp_strategy.py
+++ cybos/cp_strategy.py
@@ -35,6 +35,127 @@ class CpCssStgList(cp_util.Core):
     def get_data_value(self, data_type, index):
         self.obj.GetDataValue(data_type, index)
 
-    # hts 장내주식 현금주문 관련 데이터요청. Blocking Mode
+    # 데이터요청. Blocking Mode
     def block_request(self):
         self.obj.BlockRequest()
+
+
+# 전략에 맞는 종목 검색 클래스
+# https://money2.daishin.com/e5/mboard/ptype_basic/HTS_Plus_Helper/DW_Basic_Read_Page.aspx?boardseq=284&seq=238&page=1&searchString=%EC%A0%84%EB%9E%B5&p=8839&v=8642&m=9508
+class CpCssStgFind(cp_util.Core):
+    def __init__(self):
+        self.obj = win32com.client.Dispatch('CpSysDib.CssStgFind')
+        super(CpCssStgFind, self).__init__(self.obj)
+
+    # type 에 해당하는 입력 데이터를 value 값으로 지정합니다.
+    # type  value
+    # 0     전략 ID
+    def set_input_value(self, data_type, value):
+        self.obj.SetInputValue(data_type, value)
+
+    # type 에 해당하는 헤더데이터를 반환합니다.
+    # type  value
+    # 0     검색된 결과 종목 수
+    # 1     총 검색 종목 수
+    # 2     검색 시간
+    def get_header_value(self, data_type):
+        self.obj.GetHeaderValue(data_type)
+
+    # type 종류의 index 번째에 해당하는 데이터를 반환합니다.
+    # type  value
+    # 0     종목 코드
+    def get_data_value(self, data_type, index):
+        self.obj.GetDataValue(data_type, index)
+
+    # 데이터요청. Blocking Mode
+    def block_request(self):
+        self.obj.BlockRequest()
+
+
+# 종목검색 전략 감시 일련번호 요청 클래스
+# https://money2.daishin.com/e5/mboard/ptype_basic/HTS_Plus_Helper/DW_Basic_Read_Page.aspx?boardseq=284&seq=239&page=1&searchString=%EC%A0%84%EB%9E%B5&p=8839&v=8642&m=9508
+class CpCssWatchStgSubscribe(cp_util.Core):
+    def __init__(self):
+        self.obj = win32com.client.Dispatch('CpSysDib.CssWatchStgSubscribe')
+        super(CpCssWatchStgSubscribe, self).__init__(self.obj)
+
+    # type 에 해당하는 입력 데이터를 value 값으로 지정합니다.
+    # type  value
+    # 0     전략 ID
+    def set_input_value(self, data_type, value):
+        self.obj.SetInputValue(data_type, value)
+
+    # type 에 해당하는 헤더데이터를 반환합니다.
+    # type  value
+    # 0     해당 전략ID 감시 일련 번호
+    def get_header_value(self, data_type):
+        self.obj.GetHeaderValue(data_type)
+
+    # 데이터요청. Blocking Mode
+    def block_request(self):
+        self.obj.BlockRequest()
+
+
+# 종목검색 전략 감시 시작/중지 클래스
+# https://money2.daishin.com/e5/mboard/ptype_basic/HTS_Plus_Helper/DW_Basic_Read_Page.aspx?boardseq=284&seq=240&page=1&searchString=%EC%A0%84%EB%9E%B5&p=8839&v=8642&m=9508
+class CpCssWatchStgControl(cp_util.Core):
+    def __init__(self):
+        self.obj = win32com.client.Dispatch('CpSysDib.CssWatchStgControl')
+        super(CpCssWatchStgControl, self).__init__(self.obj)
+
+    # type 에 해당하는 입력 데이터를 value 값으로 지정합니다.
+    # type  value
+    # 0     전략 ID
+    # 1     감시 일련 번호
+    # 2     작업 구분코드 : 1 - 감시시작, 3 - 등록취소
+    def set_input_value(self, data_type, value):
+        self.obj.SetInputValue(data_type, value)
+
+    # type 에 해당하는 헤더데이터를 반환합니다.
+    # type  value
+    # 0     감시상태 : 0 - 초기상태, 1 - 감시중, 2 - 감시중단, 3 - 등록취소
+    def get_header_value(self, data_type):
+        self.obj.GetHeaderValue(data_type)
+
+    # 데이터요청. Blocking Mode
+    def block_request(self):
+        self.obj.BlockRequest()
+
+
+# 종목검색 실시간 신호 감지 클래스
+# https://money2.daishin.com/e5/mboard/ptype_basic/HTS_Plus_Helper/DW_Basic_Read_Page.aspx?boardseq=284&seq=241&page=1&searchString=%EC%A0%84%EB%9E%B5&p=8839&v=8642&m=9508
+class CpCssAlert(cp_util.Core):
+    def __init__(self):
+        self.obj = win32com.client.Dispatch('CpSysDib.CssAlert')
+        super(CpCssAlert, self).__init__(self.obj)
+
+    # type 에 해당하는 입력 데이터를 value 값으로 지정합니다.
+    # type  value
+    # 0     전략 ID
+    # 1     감시 일련 번호
+    # 2     단축종목코드
+    # 3     전입/전출 구분 : 1 - 전입, 2 - 전출
+    # 4     신호발생시각 HHMMSS(거래소 체결시세 시간)
+    # 5     현재가
+    def set_input_value(self, data_type, value):
+        self.obj.SetInputValue(data_type, value)
+
+    def subscribe(self):
+        win32com.client.WithEvents(self.obj, CpStrategyWatchHandler(self.obj))
+        self.obj.Subscribe()
+
+    def unsubscribe(self):
+        self.obj.Unsubscribe()
+
+
+# 전략주 실시간 이벤트 핸들러
+class CpStrategyWatchHandler:
+    def __init__(self, client):
+        self.client = client
+
+    def on_received(self):
+        stock = []
+        code = self.client.GetHeaderValue(2)
+        event = 'remove' if self.client.GetHeaderValue(3) is '2' else 'remain'
+        print(stock)
+        return stock
